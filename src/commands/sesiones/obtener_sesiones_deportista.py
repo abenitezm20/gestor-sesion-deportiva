dbe4915ba0e7c95837dfc704ca730da63cfb3172
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-04-23 21:29:59 -0500

    Ajuste consulta sesiones y resultados

--- src/commands/sesiones/obtener_sesiones_deportista.py
+++ src/commands/sesiones/obtener_sesiones_deportista.py
@@ -1,9 +1,9 @@
-import datetime
 import logging
 
-from sqlalchemy import func
-from datetime import datetime
+
+from sqlalchemy import text
 from src.commands.base_command import BaseCommand
+from src.models.db import db_session
 from src.errors.errors import BadRequest
 from src.models.resultado_sesion import ResultadoSesion
 from src.models.sesion import Sesion
@@ -23,47 +23,34 @@ class ObtenerSesionesDeportista(BaseCommand):
         self.fecha = info.get('fecha')
 
     def execute(self):
-        resultados = []
-
-        if self.fecha is not None:
-            logger.info(
-                f'Consultando resultado sesiones del deportista {self.email} para la fecha {self.fecha}')
-            resultados = self._consulta_por_fecha()
-        else:
-            logger.info(
-                f'Consultando resultado sesiones del deportista {self.email}')
-            resultados = self._consulta_todas()
+        resultados = self._consulta_todas()
 
         resp = []
         tmp: ResultadoSesion
         for tmp in resultados:
             resultado = {
-                'fecha_inicio': tmp.fecha_inicio.strftime("%Y-%m-%dT%H:%M:%S"),
-                'fecha_fin': tmp.fecha_fin.strftime("%Y-%m-%dT%H:%M:%S"),
+                'fecha_inicio': tmp.resultado_fecha_inicio,
+                'fecha_fin': tmp.resultado_fecha_fin,
                 'vo2_max': tmp.vo2_max,
                 'ftp': tmp.ftp,
-                'estado': tmp.sesion.estado
+                'estado': tmp.estado_sesion,
+                'fecha_sesion': tmp.fecha_sesion
             }
             resp.append(resultado)
 
         return resp
 
-    def _consulta_por_fecha(self):
-
-        filtro_fecha = datetime.fromisoformat(self.fecha)
-        anio = filtro_fecha.year
-        mes = filtro_fecha.month
-
-        resultados_mes = ResultadoSesion.query.join(Sesion).filter(
-            Sesion.email == self.email,
-            func.extract('year', Sesion.fecha_sesion) == anio,
-            func.extract('month', Sesion.fecha_sesion) == mes
-        ).all()
-
-        return resultados_mes
-
     def _consulta_todas(self):
-        resultados = ResultadoSesion.query.join(Sesion).filter(
-            Sesion.email == self.email).all()
-
-        return resultados
+        query = """
+            select
+                s.estado estado_sesion,
+                to_char(s.fecha_sesion, 'YYYY-MM-DD"T"HH24:MI:SS') fecha_sesion,
+                rs.vo2_max vo2_max,
+                rs.ftp ftp,
+                to_char(rs.fecha_inicio, 'YYYY-MM-DD"T"HH24:MI:SS') resultado_fecha_inicio,
+                to_char(rs.fecha_fin, 'YYYY-MM-DD"T"HH24:MI:SS') resultado_fecha_fin
+            from sesion s
+            left join resultado_sesion rs on (s.id = rs.id_sesion)
+            order by fecha_fin desc
+        """
+        return db_session.execute(text(query))
