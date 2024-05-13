6554a586ab320d468f6c3b9e6796c047acb5c751
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-05-13 09:53:55 -0500

    Ajuste consultas

--- src/commands/servicios/obtener_apps.py
+++ src/commands/servicios/obtener_apps.py
@@ -1,6 +1,7 @@
 import logging
 
 from src.commands.base_command import BaseCommand
+from src.models.db import db_session
 from src.errors.errors import BadRequest
 from src.models.app_externa import AppExterna
 from src.utils.str_utils import str_none_or_empty
@@ -20,22 +21,24 @@ class ObtenerApps(BaseCommand):
     def execute(self):
         logger.info('Obteniendo apps del deportista %s', self.email)
 
-        apps_deportista = AppExterna.query.filter(
-            AppExterna.email == self.email).all()
+        with db_session() as session:
 
-        resp = []
+            apps_deportista = session.query(AppExterna).filter(
+                AppExterna.email == self.email).all()
 
-        app: AppExterna
-        for app in apps_deportista:
-            app_info = {
-                'id': app.id,
-                'nombre': app.nombre,
-                'descripcion': app.descripcion,
-                'estado': app.estado.value,
-                'webhook': app.webhook,
-                'token': app.token,
-                'fecha_modificacion': app.updated_at.strftime("%Y-%m-%dT%H:%M:%S")
-            }
-            resp.append(app_info)
+            resp = []
 
-        return resp
+            app: AppExterna
+            for app in apps_deportista:
+                app_info = {
+                    'id': app.id,
+                    'nombre': app.nombre,
+                    'descripcion': app.descripcion,
+                    'estado': app.estado.value,
+                    'webhook': app.webhook,
+                    'token': app.token,
+                    'fecha_modificacion': app.updated_at.strftime("%Y-%m-%dT%H:%M:%S")
+                }
+                resp.append(app_info)
+
+            return resp
